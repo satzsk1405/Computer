61a56be6206b98a8af1dc41629d9ea5db26ebb31
satzsk1405 <satzsk1405@gmail.com>
2021-10-20 22:21:25 +0530

    PS

---
+++ src/org/computer/Computer.java
@@ -0,0 +1,9 @@
+package org.computer;
+
+public class Computer {
+public void computerModel() {
+	// TODO Auto-generated method stub
+System.out.println("computer model is hello");
+}
+
+}
---
+++ src/org/computer/Desktop.java
@@ -0,0 +1,13 @@
+package org.computer;
+
+public class Desktop extends Computer{
+private void desktopSize() {
+	// TODO Auto-generated method stub
+System.out.println("desktop type hsaiaakdo");
+}
+public static void main(String[] args) {
+	Desktop d=new Desktop();
+	d.desktopSize();
+	d.computerModel();
+}
+}
